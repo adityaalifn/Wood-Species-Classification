72446cf4ecfbd805dcf7649729deaaabe3e073b5
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-03-16 14:46:12 +0700

    add requirements.txt

--- folder.py
+++ folder.py
@@ -3,6 +3,17 @@ import os
 
 def getFolderList():
     folder_list = [name for name in os.listdir(".") if os.path.isdir(name)]
-    folder_list = [i.split("_")[1] for i in folder_list]
+    folder_list = [i.split("_")[-1] for i in folder_list]
     species_code = [i.split("_")[0] for i in folder_list]
     return folder_list, species_code
+
+DATASET_DIRECTORY = "dataset"
+def getSpeciesList():
+    idx = 0
+    species_dict = {} # strukturnya {kategori: spesies}, butuh saran struktur yang baik
+    for dir in os.listdir(DATASET_DIRECTORY):
+        species_dict[idx] = dir.split("_")[-1]
+        idx += 1
+    return species_dict
+
+print(getSpeciesList())
